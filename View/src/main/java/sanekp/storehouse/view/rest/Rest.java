140c4fc60734959242de59cc2f622fc00e22b485
SanekP <sanekne@mail.ru>
2014-06-10 17:22:11 +0300

    Specified rest root

---
+++ View/src/main/java/sanekp/storehouse/view/rest/Rest.java
@@ -0,0 +1,11 @@
+package sanekp.storehouse.view.rest;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+/**
+ * Created by sanek_000 on 6/10/2014.
+ */
+@ApplicationPath("/api")
+public class Rest extends Application {
+}
