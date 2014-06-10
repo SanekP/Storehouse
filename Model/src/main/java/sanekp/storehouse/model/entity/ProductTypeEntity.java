db59b150da66e4c370d172502e9b7d07dfca77d4
SanekP <sanekne@mail.ru>
2014-06-10 14:11:23 +0300

    Added product type entity

---
+++ Model/src/main/java/sanekp/storehouse/model/entity/ProductTypeEntity.java
@@ -0,0 +1,22 @@
+package sanekp.storehouse.model.entity;
+
+import javax.persistence.*;
+
+/**
+ * Created by sanek_000 on 6/10/2014.
+ */
+@Entity
+@Table(name = "t_product_type")
+@NamedQueries({@NamedQuery(name = "ProductType.findAll", query = "SELECT o FROM ProductTypeEntity o")})
+public class ProductTypeEntity extends AbstractEntity {
+    private String name;
+
+    @Column(nullable = false, unique = true)
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
